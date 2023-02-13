c0359377fa905c8e8d534d3feb31b41c6d7d3e05
Filip Wastman <fwastman@gmail.com>
2023-02-13 13:53:25 +0100

    Add date as template function parameter

--- includes/class-wp-operational-status-helpers.php
+++ includes/class-wp-operational-status-helpers.php
@@ -13,15 +13,67 @@ class WP_Operational_Status_Helpers {
 		global $wpdb;
 
 		$default_args = array(
-			'number_of_posts' => 10,
+			'date' => null,
+			'limit' => null,
 		);
+		$sql_parts = array(
+			'where' => array(
+				'sql' => '',
+				'arg' => ''
+			),
+			'limit' => array(
+				'sql' => '',
+				'arg' => ''
+			),
+		);
+
 		$args = wp_parse_args( $args, $default_args );
+
+		if ( $args ) {
+			foreach (  $args as $key => $value ) {
+				switch ( $key ) {
+					case 'limit':
+						if ( is_numeric( $value ) && ( $value > 0 ) ) {
+							$sql_parts['limit'] = array(
+								'sql' => 'LIMIT %d',
+								'arg' => $value,
+							);
+						}
+						break;
+					case 'date':
+						if ( is_string( $value ) && strtotime( $value ) ) {
+							$sql_parts['where'] = array(
+								'sql' => 'WHERE date_time >= %s',
+								'arg' => $value,
+							);
+						}
+						break;
+					default:
+				}
+			}
+		}
+
 		$wpdb->operational_status_log = $wpdb->prefix . WP_OPERAIONAL_STATUS_DB_TABLE_PREFIX . '_log';
+
+		$sql = sprintf(
+			"SELECT * FROM $wpdb->operational_status_log %s ORDER BY id DESC %s",
+			$sql_parts['where']['sql'],
+			$sql_parts['limit']['sql']
+		);
+
+		$sql_args = array_filter( wp_list_pluck( $sql_parts, 'arg' ) );
+
+		$result = $wpdb->get_results(
+			$wpdb->prepare(
+				$sql,
+				$sql_args
+			)
+		);
 
 		return $wpdb->get_results(
 			$wpdb->prepare(
-				"SELECT * FROM $wpdb->operational_status_log ORDER BY id DESC LIMIT %d" ,
-				$args['number_of_posts']
+				$sql,
+				$sql_args
 			)
 		);
 	}
